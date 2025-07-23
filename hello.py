19e6c3539273a2b3c6bc1474d67129e31d591a8b
Musiki8213 <sithomolamusiki@gmail.com>
2025-07-23 23:07:57 +0200

    added python

---
+++ hello.py
@@ -0,0 +1,6 @@
+fName = input("Please enter your first name ")
+sName = input("Please enter your surname ")
+
+
+
+print(f"Hello {fName.capitalize()} {sName.capitalize()} I hope you have a wonderful day!")
